db9936eb989e9d0682f88c7233954856314019f1
Bruno Felix <brunofelix120@gmail.com>
2024-09-09 18:08:24 -0300

    Mudanças 2.0

--- StartDB2024/src/analisaRecintos.js
+++ StartDB2024/src/analisaRecintos.js
@@ -1,4 +1,4 @@
-// analisaRecintos.js
+// encontrarRecinto.js
 
 import { RecintosZoo } from './recintos-zoo.js';
 
@@ -7,11 +7,11 @@ const zoo = new RecintosZoo();
 // Teste para MACACO, 2
 console.log(zoo.encontrarRecinto('MACACO', 2));
 
-// Teste para BICHO PAPÃO, 1 (animal inválido)
-//console.log(zoo.analisaRecintos('BICHO PAPÃO', 1));
+ //Teste para BICHO PAPÃO, 1 (animal inválido)
+console.log(zoo.encontrarRecinto('BICHO PAPÃO', 1));
 
 // Teste para LEAO, 1
-//console.log(zoo.analisaRecintos('LEAO', 1));
+console.log(zoo.encontrarRecinto('LEAO', 1));
 
 // Teste para HIPOPOTAMO, 2
-//console.log(zoo.analisaRecintos('HIPOPOTAMO', 2));
+//console.log(zoo.encontrarRecinto('HIPOPOTAMO', 2));
